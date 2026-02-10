331ddcf24e0d82632936b170582d3df69170dc36
henrygranados <henrygranados03@gmail.com>
2026-02-10 13:28:56 -0700

    more changes

--- src/utils/stackOverflowCache.ts
+++ src/utils/stackOverflowCache.ts
@@ -7,27 +7,27 @@ interface StackOverflowData {
   };
 }
 
-//let cachedData: StackOverflowData | null = null;
+let cachedData: StackOverflowData | null = null;
 
 export const getStackOverflowData =
   async (): Promise<StackOverflowData | null> => {
-    // if (cachedData) {
-    //   return cachedData;
-    // }
+    if (cachedData) {
+      return cachedData;
+    }
 
-    // try {
-    //   const res = await fetch(
-    //     "https://api.stackexchange.com/2.2/users/4084160?order=desc&sort=reputation&site=stackoverflow",
-    //   );
-    //   const data = await res.json();
+    try {
+      const res = await fetch(
+        "https://api.stackexchange.com/2.2/users/4084160?order=desc&sort=reputation&site=stackoverflow",
+      );
+      const data = await res.json();
 
-    //   if (data.items && data.items[0]) {
-    //     cachedData = data.items[0];
-    //     return cachedData;
-    //   }
-    // } catch (error) {
-    //   console.error("Error fetching StackOverflow data:", error);
-    // }
+      if (data.items && data.items[0]) {
+        cachedData = data.items[0];
+        return cachedData;
+      }
+    } catch (error) {
+      console.error("Error fetching StackOverflow data:", error);
+    }
 
     return null;
   };
